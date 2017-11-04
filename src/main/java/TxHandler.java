780dc718410ac478155fa8b954b58cd2ba68cb70
PDXBD <andrea@scasso.com>
2017-11-04 03:57:40 +0000

    Add constructor

--- src/main/java/TxHandler.java
+++ src/main/java/TxHandler.java
@@ -5,15 +5,19 @@ public class TxHandler {
      * {@code utxoPool}. This should make a copy of utxoPool by using the UTXOPool(UTXOPool uPool)
      * constructor.
      */
+
+    private UTXOPool pool;
+
     public TxHandler(UTXOPool utxoPool) {
-        // IMPLEMENT THIS
+        // IMPLEMENTED
+        this.pool = new UTXOPool(utxoPool);
         return;
     }
 
     /**
      * @return true if:
-     * (1) all outputs claimed by {@code tx} are in the current UTXO pool, 
-     * (2) the signatures on each input of {@code tx} are valid, 
+     * (1) all outputs claimed by {@code tx} are in the current UTXO pool,
+     * (2) the signatures on each input of {@code tx} are valid,
      * (3) no UTXO is claimed multiple times by {@code tx},
      * (4) all of {@code tx}s output values are non-negative, and
      * (5) the sum of {@code tx}s input values is greater than or equal to the sum of its output
